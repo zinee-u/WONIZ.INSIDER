dd249cb381ae6b60ff4d733922474d85d521c7b0
zinee-u <suzinee.u@gmail.com>
2023-09-06 22:30:03 +0900

    first commit

---
+++ Insider/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Insider
+//
+//  Created by 유수진 on 2023/09/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Insider/InsiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsiderApp.swift
+//  Insider
+//
+//  Created by 유수진 on 2023/09/06.
+//
+
+import SwiftUI
+
+@main
+struct InsiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
