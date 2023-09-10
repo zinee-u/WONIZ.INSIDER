64bb1d6e9ad8eebae19d9f6fb7e8dd13e644129a
zinee-u <suzinee.u@gmail.com>
2023-09-11 03:46:20 +0900

    Add Database and Storage

--- Insider/ContentView.swift
+++ Insider/ContentView.swift
@@ -4,11 +4,27 @@
 //
 //  Created by 유수진 on 2023/09/06.
 //
+//  Keywords: Firebase
+//  Ref.
+//  - https://ios-development.tistory.com/769
+//  - https://hryang.tistory.com/32
+//  - https://makeschool.org/mediabook/oa/tracks/build-ios-apps/build-a-photo-sharing-app/uploading-photos-to-firebase/
+//  - https://fomaios.tistory.com/entry/Swift-Storage%EC%97%90%EC%84%9C-%EC%9D%B4%EB%AF%B8%EC%A7%80-%EC%97%85%EB%A1%9C%EB%93%9C-%EB%B0%8F-%EB%8B%A4%EC%9A%B4%EB%A1%9C%EB%93%9C%ED%95%98%EA%B8%B0
+//  - https://velog.io/@ayb226/Flutter-%EC%98%A4%EB%A5%98-%EB%AA%A8%EC%9D%8C-FIRMessaging-Remote-Notifications-proxy-enabled-%ED%95%B4%EA%B2%B0%EB%B2%95
+//  - https://github.com/you6878/howltalk_ios_simple_version/blob/master/Guide/section_4/index.md
+//  - https://designcode.io/swiftui-advanced-handbook-imagepicker
 
 import SwiftUI
+import UIKit
 import KakaoSDKCommon
 import KakaoSDKAuth
 import KakaoSDKUser
+import UIKit
+import Firebase
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+import FirebaseStorage
 
 struct ContentView: View {
     var body: some View {
@@ -58,8 +74,11 @@ struct SigninKakao: View{
 
 /* VStack.Kakao 화면 */
 struct SigninPage: View{
+    
+    @State private var image = UIImage()
+    @State private var showSheet = false
+    
     var body: some View {
-        
         VStack {
             Text("KakaoTalk")
                 .foregroundColor(.purple)
@@ -72,6 +91,7 @@ struct SigninPage: View{
                         print(oauthToken)
                         print("error=")
                         print(error)
+                        
                     }
                 } else {
                     /* 카톡 미 설치경우 사파리를 통한 로그인 */
@@ -83,7 +103,53 @@ struct SigninPage: View{
                     }
                 }
             }, label: {
+                Text("Sign Up")
+                    .foregroundColor(.purple)
+                    .background(Color.black)
+            })
+            
+            Button(action:{
+                let testItemsReference = Database.database().reference(withPath: "test-items")
+                let userItemRef = testItemsReference.child("user")
+                let values: [String: Any] = [ "age": 30, "name": "usuzin"]
+                userItemRef.setValue(values)
+            }, label: {
                 Text("Sign In")
+                    .foregroundColor(.purple)
+                    .background(Color.black)
+            })
+            
+            Button(action: {
+                self.showSheet.toggle()
+                
+            }, label: {
+                Text("Select Pic.")
+                    .foregroundColor(.purple)
+                    .background(Color.black)
+            })
+            .sheet(isPresented: $showSheet) {
+                ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
+            }
+            
+            Button(action: {
+                let default_image = UIImage(named: "Default_Img")
+                let d_image = default_image?.jpegData(compressionQuality: 0.5)
+                let u_image = image.jpegData(compressionQuality: 0.5)
+                let data = (u_image ?? d_image)!
+                let storage = Storage.storage()
+                let filePath = "Users_Pics/user1"
+                let metaData = StorageMetadata()
+                metaData.contentType = "image/png"
+                storage.reference().child(filePath).putData(data, metadata: metaData){
+                     (metaData,error) in if let error = error {
+                         print(error.localizedDescription)
+                         return
+                     } else {
+                         print("Succeeded!")
+                     }
+                }
+            }, label: {
+                Text("Upload Pic.")
                     .foregroundColor(.purple)
                     .background(Color.black)
             })
@@ -92,3 +158,48 @@ struct SigninPage: View{
         .background(Color.black)
     }
 }
+
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) private var presentationMode
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @Binding var selectedImage: UIImage
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+
+        return imagePicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+        var parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+
+    }
+}
+
